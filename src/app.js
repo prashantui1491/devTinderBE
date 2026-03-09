afeefcfd8c8f0ae6c6a2e8c591cf382b66e9f11d
Prashant Balappa Kumbar <pkumbar@vaeso.com>
2026-03-10 05:14:20 +0530

    password encryption

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const connectDb = require("./config/database");
-
 const app = express();
-
 const User = require("./models/user");
+const { validateSignupData } = require("./utils/validate");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
@@ -12,11 +12,48 @@ app.post("/signup", async (req, res) => {
   //console.log("reqbody", req.body);
 
   try {
-    const user = new User(req.body);
+    // validate signup data
+    validateSignupData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // encrypt password
+    const passwordHash = await bcrypt.hash(password, 10);
+    //console.log("passwordHash", passwordHash)
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await user.save();
     res.send("user added successfully");
   } catch (err) {
-    res.status(500).send("Error saving the repsonce" + err.message);
+    res.status(500).send("Error saving the repsonce " + err.message);
+  }
+});
+
+//Login api
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+
+    const ispasswordValid = await bcrypt.compare(password, user.password);
+
+    if (ispasswordValid) {
+      res.send("Logged in sucessuly !!!!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
   }
 });
 
@@ -76,16 +113,10 @@ app.delete("/user", async (req, res) => {
 app.patch("/user/:userId", async (req, res) => {
   const userId = req?.params?.userId;
   const data = req.body;
-  console.log("datacheck",data.skills)
+  console.log("datacheck", data.skills);
 
   try {
-    const ALLOWED_UPDATES = [
-      "photoUrl",
-      "about",
-      "gender",
-      "age",
-      "skills",
-    ];
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
 
     const isUpdateAllowed = Object.keys(data).every((k) =>
       ALLOWED_UPDATES.includes(k),
