29987d41bc2a626bcbddf0da63946333785538b7
Prashant Balappa Kumbar <pkumbar@vaeso.com>
2026-03-15 07:42:39 +0530

    reset password api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -4,7 +4,9 @@ const profileRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 
-const { validateEditProfileData } = require("../utils/validate");
+const { validateEditProfileData, validatePasswordChange } = require("../utils/validate");
+
+const bcrypt = require("bcrypt");
 
 // get profie api
 
@@ -32,6 +34,35 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.json({
       message: `${loggedinUser.firstName}, your profile updated successfully`,
       data: loggedinUser,
+    });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+// Change password API
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    validatePasswordChange(req);
+
+    const user = req.user;
+    const { oldPassword, newPassword } = req.body;
+
+    // Verify old password
+    const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error("Old password is incorrect");
+    }
+
+    // Hash new password
+    const hashPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = hashPassword;
+    await user.save();
+
+    res.json({
+      message: "Password updated successfully",
     });
   } catch (err) {
     res.status(400).send("Error: " + err.message);
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -18,9 +18,22 @@ const validateEditProfileData = (req)=>{
   const isAllowedEdit  = Object.keys(req.body).every(field => allowedEditeFields.includes(field))
 
   return isAllowedEdit
-
-
-
 }
 
-module.exports = {validateSignupData, validateEditProfileData}
+const validatePasswordChange = (req) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword) {
+    throw new Error("Old password and new password are required");
+  }
+
+  else if(oldPassword === newPassword){
+    throw new Error("old and new password are same")
+  }
+  
+  else if (!validator.isStrongPassword(newPassword)) {
+    throw new Error("New password must be strong");
+  }
+}
+
+module.exports = {validateSignupData, validateEditProfileData, validatePasswordChange}
